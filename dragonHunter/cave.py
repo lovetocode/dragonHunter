18bd2750d4f0f716080d81032cfc1bc5c0083418
Daniel <cranky30@hotmail.com>
2020-08-21 19:49:59 +0800

    Finished first version of DragonHunter

--- dragonHunter/cave.py
+++ dragonHunter/cave.py
@@ -1,22 +1,47 @@
 # There are the classes and functions for the cave. 
 
-# none -> string
-# output what happens in room one
-# def roomOne()
-def roomOne():
-	print("You are now in room one. The dragon is in the room. Now you need to capture the dragon.")
-	print("1 lunge at the dragon")
-	print("2 attempt to talk the dragon and take him peacefully.")
-	print("3 Tell the dragon a joke to catch him off gard and arrest him.")
-	print("4 challenge him to a fight.")
-	choice = input("<")
-	if choice == "1":
-		print("The dragon blocks you. You are dead game over.")
-	elif choice == "2" :
-		print("All talk and no action makes for a dead dragon humter.")
-	elif choice == "3":
-		print("Dragons have no sense of hunor. You are dead game over.")
-	elif choice == "4":
-		print("You have no chance. BUt, you win anyway and capture the dragon. You win.")
+def caveEntrance():
+	print("There are 3 ways you can go. l for left")
+	print("r for right and f for forward")
+	direction = input(">")
+	if direction == "r":
+		caveR()
+	elif direction == "l":
+		caveL()
+	elif direction == "f":
+		caveC()
+	else:
+		print("computer does not understand")
+		
+def caveR():
+	print("You have been eaten by the dragon.")
+	print("Do you want to play again. y or n")
+	play = input(">")
+	playAgain(play)
+def caveL():
+	print("You have been eaten by the dragon")
+	print("Do you want to play again. y or n")
+	play = input(">") 
+	playAgain(play)
+def caveC():
+	print("You have been eaten by the dragon")
+	print("Do you want to play again. y or n")
+	play = input(">")
+	playAgain(play)
+def playAgain(p):
+	if p == 'y':
+		intro()
+	elif p == 'n':
+		print("Thank you for playing.")
 
-roomOne()
+def intro():
+	print("Hello welcome to Dragon hunter version 1.0 Beta.")
+	print("Press s to start the game")
+	s = input(">")
+	if s == "s":
+		caveEntrance()
+	else:
+		intro()
+		
+intro()
+	
